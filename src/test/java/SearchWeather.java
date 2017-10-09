c113f14aae6d2c22f15e40657b33172e6cd5294c
belphegor89 <yura.zosin@gmail.com>
2017-10-09 15:43:46 +0300

    minor changes

--- src/test/java/SearchWeather.java
+++ src/test/java/SearchWeather.java
@@ -1,11 +1,14 @@
 import Pages.ForecastPage;
 import Utils.BaseTest;
+import org.apache.log4j.Logger;
 import org.testng.annotations.Test;
 
 /**
  * Created by yzosin on 20-Sep-17.
  */
 public class SearchWeather extends BaseTest {
+
+    public static Logger logger = Logger.getLogger(SearchWeather.class);
 
     @Test
     public void getForecast() throws Exception {
--- src/test/java/SendReportInMail.java
+++ src/test/java/SendReportInMail.java
@@ -1,12 +1,15 @@
 import Pages.LoginPage;
 import Pages.SendMail;
 import Utils.BaseTest;
+import org.apache.log4j.Logger;
 import org.testng.annotations.Test;
 
 /**
  * Created by owner on 05-Oct-17.
  */
 public class SendReportInMail extends BaseTest {
+
+    public static Logger logger = Logger.getLogger(SendReportInMail.class);
 
     @Test
     public void loginToEmail(){
